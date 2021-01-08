890fc26761d7c84dc79eb2634a88441cfccbd9a9
Finettetaart <77108742+Finettetaart@users.noreply.github.com>
2021-01-08 23:21:43 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+var name = ""
+function showname()
+{
+    if(document.form.name.value == "")
+    {
+        alert("U heeft geen naam ingevoerd. Vul alstublieft uw naam in of klik op geen interesse.");
+    }
+    else        
+    {
+        var name = " " + document.form.name.value;
+        alert("U heeft de naam" + name + " ingevult. Vul hem nog een keer in om hem te wijzigen");
+        document.getElementById("viewname").innerHTML = name;
+    }
+}
+function geeninteresse()
+{
+    if (name = "")
+    alert("U heeft geen naam ingevoerd. Vul hem alsnog in om hem te wijzigen.")
+    else
+    var name = ""
+    alert("U heeft uw naam gereset, vul hem nog een keer in om hem te wijzigen.")
+    document.getElementById("viewname").innerHTML = name; 
+}
